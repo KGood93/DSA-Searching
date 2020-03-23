fc04bd933e18511a7328c23e0a34f14c561ec8e6
KGood93 <goodreau.kath@gmail.com>
2020-03-23 14:42:59 -0500

    First commit

---
+++ Course.js
@@ -0,0 +1,76 @@
+//Linear Search - loop through array 1-by-1 until you find what you are looking for
+function indexOf(array, value) {
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+//Binary Search - dividing the range in half each time (divide and conquer)
+function binarySearch(array, value, start, end) {
+    var start = start === undefined ? 0 : start;
+    var end = end === undefined ? array.length : end;
+
+    if(start > end) {
+        return -1;
+    }
+
+    const index = Math.floor((start + end) / 2);
+    const item = array[index];
+
+    console.log(start, end);
+    if(item == value) {
+        return index;
+    }
+    else if (item < value) {
+        return binarySearch(array, value, index + 1, end);
+    }
+    else if (item > value) {
+        return binarySearch(array, value, start, index - 1);
+    }
+};
+
+//Depth-first Search - Down a binary search tree. Breadth-first search - across a binary search tree.
+class BinarySearchTree {
+    constructor(key = null, value = null, parent = null) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+
+    dfs(values=[]) {
+        if(this.left) {
+            values = this.left.dfs(values);
+        }
+        values.push(this.value);
+
+        if(this.right) {
+            values = this.right.dfs(values);
+        }
+        return values;
+    }
+
+    bfs(tree, values = []) {
+        const queue = new queue(); //Assuming a queue is implemented (refer to previous lesson on Queue)
+        const node = tree.root;
+        queue.enqueue(node);
+        while (queue.length) {
+            const node = queue.dequeue(); //remove from the queue
+            values.push(node.value); //add that value from the queue to an array
+
+            if (node.left) {
+                queue.enqueue(node.left); //add left child to the queue
+            }
+
+            if(node.right) {
+                queue.enqueue(node.right); //add right child to the queue
+            }
+        }
+
+        return values;
+    }
+}
